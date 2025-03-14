fbe16a8d63bccf60e82bfd17a8b9f31d88a492e7
bosbobos <konovalov.matvej@mail.ru>
2025-03-14 13:58:17 +0300

    Fixed the code, it fully works now!

--- ElGamal.py
+++ ElGamal.py
@@ -1,6 +1,6 @@
 import Utils as u
 import random
-from math import floor, ceil
+from math import floor, ceil, log2
 
 class ElGamal:
     def __init__(self, p, g):
@@ -22,15 +22,16 @@ class ElGamal:
     def decrypt_message(self, encrypted):
         message = ''
         for c1, c2 in encrypted:
-            m = c2 // (u.mod_pow(c1, self.__private_key, self.p)) % self.p
+            m = c2 * u.mod_inv(u.mod_pow(c1, self.__private_key, self.p), self.p) % self.p
             bin_m = bin(m)[2:]
-            message += u.binary_to_string(bin_m.zfill(ceil(len(bin_m))*8))
+            message += u.binary_to_string(bin_m.zfill(ceil(len(bin_m)/8)*8))
         return message
 
     def encrypt_message(self, message, public_key):
         res = []
         bin_msg = u.string_to_binary(message)
-        bytes_in_block = floor(self.p / 255)
+        bits_in_block = floor(log2(self.p))
+        bytes_in_block = floor(bits_in_block / 8)
         block_num = ceil(len(bin_msg) / 8 / bytes_in_block)
         for i in range(block_num):
             block = bin_msg[i * 8 * bytes_in_block:(i+1) * 8 * bytes_in_block]
@@ -48,7 +49,7 @@ g = u.get_g(p)
 a = ElGamal(p, g)
 b = ElGamal(p, g)
 
-message = 'hello'
+message = 'hello! how are you doing today? hope everything\'s ok!'
 encrypted = a.encrypt_message(message, b.get_public_key())
 decrypted = b.decrypt_message(encrypted)
 print(decrypted)
